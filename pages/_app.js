bcdf400ce23fef83d4feab349c2d4004e62afeea
Ryan Scherler <ryan@eastslope.studio>
2020-07-30 21:13:30 -0700

    add fonts and org deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/index.css'
+import 'typeface-roboto'
+import 'typeface-ibm-plex-mono'
+
+import 'styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
