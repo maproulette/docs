bb85da42f04d030060883cff04532f5abb34bc79
Ryan Scherler <ryan@eastslope.studio>
2020-08-13 16:38:27 -0700

    parse markdown body in releases

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,5 +1,10 @@
 import 'focus-visible'
 import 'alpinejs'
+import markdownIt from 'markdown-it'
+
+window.markdown = markdownIt({
+  html: true,
+})
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
